a711b2b3574d046b15a1eb64045690331f3d00b0
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-01-25 23:51:28 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,8 +1,10 @@
 function solution(n) {
     const answer = [];
     for(let count = 0; n >= count; count++) {
-        if(count % 2 == 1)
+        if(count % 2 === 1)
             answer.push(count);
     }
     return answer;
 }
+// 그냥 인덱스 2개씩 증가시켜서 홀수만 push 하는 방법도 있다.
+// filter를 활용하는 방법도 있다.
