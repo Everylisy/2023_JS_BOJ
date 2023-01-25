2e57f7340216e56366f41691247c854ec875e50c
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-01-25 23:28:43 +0900

    Update 나머지.js

--- 백준/Bronze/10430. 나머지/나머지.js
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -1,10 +1,10 @@
-let input = require("fs")
+const input = require('fs')
   .readFileSync('/dev/stdin')
   .toString()
-  .split(" ");
-var A = parseInt(input[0]);
-var B = parseInt(input[1]);
-var C = parseInt(input[2]);
+  .split(' ')
+  .map(Number);
+
+const [A, B, C] = [input[0], input[1], input[2]];
 console.log((A + B) % C);
 console.log(((A % C) + (B % C)) % C);
 console.log((A * B) % C);
