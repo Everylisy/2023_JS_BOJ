94f63c1dae679ceef4c3f8fdb7b7b743804aaaff
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-01-26 20:44:48 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,7 @@
+const input = +require('fs').readFileSync('/dev/stdin');
+
+if (input <= 100 && input >= 90) console.log('A');
+else if (input <= 89 && input >= 80) console.log('B');
+else if (input <= 79 && input >= 70) console.log('C');
+else if (input <= 69 && input >= 60) console.log('D');
+else console.log('F');
