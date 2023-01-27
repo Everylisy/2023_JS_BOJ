da3287d5b6b7d14920e34e6ac226f24e24a462ca
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-01-27 23:58:40 +0900

    [level 0] Title: 최빈값 구하기, Time: 6.23 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -0,0 +1,15 @@
+function solution(array) {
+    if (array.length === 1) return array[0];
+    const sortedArr = array.sort((a, b) => a - b);
+
+    let obj = {};
+    sortedArr.forEach((el) => {
+        if (!obj[el]) obj[el] = 1;
+        else obj[el]++;
+    });
+
+    const arr = Object.entries(obj).sort((a, b) => b[1] - a[1]);
+    console.log(arr);
+    if (arr.length > 1 && arr[0][1] === arr[1][1]) return -1;
+    return +arr[0][0];
+}
