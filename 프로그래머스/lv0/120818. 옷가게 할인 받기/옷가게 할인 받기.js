0001b8384bbd7b42ce06f5ac4b31e458a4c2d22e
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-01-28 18:01:52 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,9 @@
+function getSalePrice(percent, price) {
+    return parseInt(price - (price * (percent / 100)));
+}
+function solution(price) {
+    if(500000 <= price) return getSalePrice(20, price);
+    else if (300000 <= price) return getSalePrice(10, price);
+    else if (100000 <= price) return getSalePrice(5, price);
+    else return price;
+}
