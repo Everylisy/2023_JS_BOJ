8486e77ccfe55cb9d02cd97993a588e9454ebcae
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-01-31 16:36:23 +0900

    [Bronze II] Title: 다이얼, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.js
+++ 백준/Bronze/5622. 다이얼/다이얼.js
@@ -1,25 +1,24 @@
-const input = require("fs")
-  .readFileSync(__dirname + "dev/stdin")
+const input = require('fs')
+  .readFileSync('/dev/stdin')
   .toString()
-  .trim();
+  .trim()
+  .split('');
 
 const dial = {
-  2: "ABC",
-  3: "DEF",
-  4: "GHI",
-  5: "JKL",
-  6: "MNO",
-  7: "PQRS",
-  8: "TUV",
-  9: "WXYZ",
+  2: 'ABC',
+  3: 'DEF',
+  4: 'GHI',
+  5: 'JKL',
+  6: 'MNO',
+  7: 'PQRS',
+  8: 'TUV',
+  9: 'WXYZ',
 };
-let sec = 0;
 
-for (let i = 0; i < input.length; i++) {
-  for (let j = 2; j <= 9; j++) {
-    if (dial[j].includes(input[i])) {
-      sec += j + 1;
-    }
+let time = 0;
+input.forEach((el) => {
+  for (let j = 2; 9 >= j; j++) {
+    if (dial[j].includes(el)) time += j + 1;
   }
-}
-console.log(sec);
+});
+console.log(time);
