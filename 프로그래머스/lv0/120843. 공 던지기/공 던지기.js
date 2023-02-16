f8fa27abc9e1f320f7e12401908d5eb8fe3c40c4
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-02-16 15:01:20 +0900

    [level 0] Title: 공 던지기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -1,4 +1,14 @@
+// function solution(numbers, k) {
+//     const len = numbers.length;
+//     return numbers[(((k - 1) * 2) % len)];
+// }
+
+// 반복문으로도 해결 가능
 function solution(numbers, k) {
-    const len = numbers.length;
-    return numbers[(((k - 1) * 2) % len)];
+    let cnt = 1;
+    for (let i = 0;;i += 2) {
+        i %= numbers.length;
+        if (cnt === k) return numbers[i];
+        cnt++;
+    }
 }
