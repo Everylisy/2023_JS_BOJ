0b16beabc035dfb0ae0eb197888a7b690b285f60
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-02-16 15:10:54 +0900

    [level 0] Title: 가까운 수, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,5 +1,11 @@
 function solution(array, n) {
-    const minus = array.sort((a, b) => a - b).map(val => [val,  Math.abs(n - val)]);
-    const sorted = minus.sort((a, b) => a[1] - b[1]);
-    return sorted[0][0];
+    // const minus = array.sort((a, b) => a - b).map(val => [val,  Math.abs(n - val)]);
+    // const sorted = minus.sort((a, b) => a[1] - b[1]);
+    // return sorted[0][0];
+    
+    // 내가 다시 풀어본 방법
+    const mapArr = array.sort((a, b) => a - b).map(val => Math.abs(n - val));
+    const sorted = [...mapArr].sort((a, b) => a - b);
+    const index = mapArr.indexOf(sorted[0]);
+    return array[index];
 }
