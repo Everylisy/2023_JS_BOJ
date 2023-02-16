7dd162806edc68f3e8617098b1a369f3e198e93d
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-02-16 13:19:17 +0900

    [level 0] Title: 가까운 수, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -3,8 +3,3 @@ function solution(array, n) {
     const sorted = minus.sort((a, b) => a[1] - b[1]);
     return sorted[0][0];
 }
-// function solution(array, n) {
-//     const mapArr = array.sort((a, b) => a - b).map((val) => [val, Math.abs(n - val)]);
-//     const sortedArr = mapArr.sort((a, b) => a[1] - b[1]);
-//     return sortedArr[0][0];
-// }
