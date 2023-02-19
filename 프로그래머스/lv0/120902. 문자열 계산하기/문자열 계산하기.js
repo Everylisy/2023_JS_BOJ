f7293d41a75e59fb2fde3f3adabea4ab77d7cc30
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-02-19 22:03:49 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -5,18 +5,5 @@ function solution(my_string) {
         if (arr[i] === '+') result += Number(arr[i + 1]);
         else if (arr[i] === '-') result -= Number(arr[i + 1]);
     }
-    return result
+    return result;
 }
-
-// function solution(my_string) {
-//     const arr = my_string.split(' + ');
-//     let result = 0;
-//     for (let i = 0; arr.length > i; i++) {
-//         if (arr[i].length === 1) result += Number(arr[i]);
-//         else {
-//             const minus = arr[i].split(' - ').map(Number);
-//             result += minus[0] - minus[1];
-//         }
-//     }
-//     return result;
-// }
