1151283c9c93083c0b0c2982155fa2b91f1a3944
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-02-27 20:19:33 +0900

    [level 0] Title: 외계어 사전, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,7 +1,9 @@
 function solution(spell, dic) {
-    let answer = 2;
-    dic.forEach((el) => {
-        spell.every(text => el.includes(text)) ? answer = 1 : null;
+    const answer = dic.filter((el) => {
+        return spell.every(text => el.includes(text));
     });
-    return answer;
+
+    // ["ppooss"] 와 같이 "한번씩만" 사용되지 않는 경우에는 2를 반환하도록 수정
+    const answerLen = answer.toString().split('').length;
+    return answerLen === spell.length ? 1 : 2;
 }
