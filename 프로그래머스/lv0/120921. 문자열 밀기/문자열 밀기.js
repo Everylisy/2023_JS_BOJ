a934dba5f413258241897d9e8ee0b0896c9bc66f
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-03-07 11:05:21 +0900

    [level 0] Title: 문자열 밀기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,10 +1,9 @@
 function solution(A, B) {
     if (A === B) return 0;
-    const aArr = A.split('');
+    const aArr = [...A];
 
     for (let i = 0; A.length > i; i++) {
         aArr.unshift(aArr.pop());
-        console.log(A);
         if (aArr.join('') === B) return i + 1;  
     }
     return -1;
