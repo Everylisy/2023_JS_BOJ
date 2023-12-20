e0f4974b20edbace6be856092fd90219186d39ed
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-12-20 21:45:32 +0900

    [level unrated] Title: a와 b 출력하기, Time: 51.39 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.js
+++ 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.js
@@ -2,14 +2,8 @@ const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
-
-let input = [];
-
-rl.on('line', function (line) {
-    input = line.split(' ');
-}).on('close', function () {
-    const [a, b] = [...input];
-    console.log(`a = ${a}
-b = ${b}`);
+}).on('line', function (line) {
+    const [a, b] = line.split(" ");
+    console.log(`a = ${a}`);
+    console.log(`b = ${b}`);
 });
