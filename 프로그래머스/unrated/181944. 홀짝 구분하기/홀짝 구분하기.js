7133b808047f54809d904a49eaa828b9244f0514
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-12-21 10:10:08 +0900

    [level unrated] Title: 홀짝 구분하기, Time: 70.12 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.js
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -3,6 +3,6 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 }).on('line', function (line) {
-    const answer = +line % 2 ? 'odd' : 'even';
-    console.log(line, 'is', answer);
+    const answer = line % 2 === 0 ? 'even' : 'odd';
+    console.log(`${line} is ${answer}`);
 })
