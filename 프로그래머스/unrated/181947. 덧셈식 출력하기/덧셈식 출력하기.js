9a03915d7b0fbc8bf39530f06b65d6d23c4618a5
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-12-21 10:03:14 +0900

    [level unrated] Title: 덧셈식 출력하기, Time: 45.86 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181947. 덧셈식 출력하기/덧셈식 출력하기.js
+++ 프로그래머스/unrated/181947. 덧셈식 출력하기/덧셈식 출력하기.js
@@ -2,13 +2,7 @@ const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
-
-let input = [];
-
-rl.on('line', function (line) {
-    input = line.split(' ');
-}).on('close', function () {
-    const [a, b] = [...input];
+}).on('line', function(line) {
+    const [a, b] = line.split(' ');
     console.log(`${a} + ${b} = ${+a + +b}`);
-});
+})
