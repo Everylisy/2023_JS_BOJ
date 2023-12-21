c104ed4de1d2e4dc4253c403df8ed82a12c5c4d6
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-12-21 10:04:34 +0900

    [level unrated] Title: 문자열 붙여서 출력하기, Time: 70.25 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
+++ 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
@@ -2,14 +2,6 @@ const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
-
-let input = [];
-
-rl.on('line', function (line) {
-    input = line.split(' ');
-}).on('close', function () {
-    str1 = input[0];
-    str2 = input[1];
-    console.log(str1 + str2);
-});
+}).on('line', function(line) {
+    console.log(line.split(' ').join(''));
+})
