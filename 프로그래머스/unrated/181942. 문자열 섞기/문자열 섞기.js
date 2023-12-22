60e9552b61621c6694e173d965f5c3a8a0962dca
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-12-22 15:55:55 +0900

    [level unrated] Title: 문자열 섞기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
@@ -1,4 +1,4 @@
 function solution(str1, str2) {
-    const answer = [...str1].map((val, idx) => val + str2[idx]).join('');
-    return answer;
+    const answer = [...str1].map((val, idx) => val + str2[idx]);
+    return answer.join('');
 }
