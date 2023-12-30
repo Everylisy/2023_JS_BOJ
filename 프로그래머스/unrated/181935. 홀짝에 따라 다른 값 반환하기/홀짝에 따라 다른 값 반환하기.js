c3822e7a30990f7d9e98fd54bc733f3df7a4151c
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-12-30 12:12:30 +0900

    [level unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -1,16 +1,10 @@
 function solution(n) {
+    let [evenSum, oddSum] = [0, 0];
     const isEven = n % 2 === 0;
-    const result = [];
-    
-    if (isEven) {
-        for (let i = 1; n >= i; i++) {
-            if (i % 2 === 0) result.push(i);
-        };
-        return result.reduce((acc, cur) => acc + cur**2, 0);
-    } else {
-        for (let i = 1; n >= i; i++) {
-            if (i % 2 === 1) result.push(i);
-        };
-        return result.reduce((acc, cur) => acc + cur, 0);
+
+    for (let i = 1; i <= n; i++) {
+        if (i % 2 === 0) evenSum += Math.pow(i, 2);
+        else oddSum += i;
     }
+    return isEven ? evenSum : oddSum;
 }
