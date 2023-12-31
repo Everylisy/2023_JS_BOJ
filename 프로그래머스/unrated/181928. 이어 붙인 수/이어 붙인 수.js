3dd6c4d5c8f77494efa8ddc65df27ad5f94b1473
이영우 <60170829+Everylisy@users.noreply.github.com>
2023-12-31 10:39:05 +0900

    [level unrated] Title: 이어 붙인 수, Time: 0.05 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -1,5 +1,9 @@
 function solution(num_list) {
-    const even = num_list.filter(el => el % 2 === 0).join('');
-    const odd = num_list.filter(el => el % 2 === 1).join('');
-    return +even + +odd;
+    let [oddSum, evenSum] = ['', ''];
+    
+    for (const num of num_list) {
+        num % 2 === 0 ? evenSum += num : oddSum += num;
+    }
+    
+    return +oddSum + +evenSum;
 }
